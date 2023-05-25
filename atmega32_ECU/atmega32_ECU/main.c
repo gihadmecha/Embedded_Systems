ab4cbd7572ce2a49dcc1383483b1a6bc76eb315a
gihadmecha <gihadmecha@gmail.com>
2023-05-25 16:17:22 +0300

    organization

--- atmega32_ECU/atmega32_ECU/main.c
+++ atmega32_ECU/atmega32_ECU/main.c
@@ -1,19 +1,8 @@
 
 
-#include "SEGMENTS.h"
+
 
 int main ()
 {
-	SEGMENTS4_2_Init ();
 	
-	while (1)
-	{
-		for (u8 number = 0; number <= 99; number++)
-		{
-			for (u8 count = 0; count <= 100; count++)
-			{
-				SEGMENTS4_2 (number);
-			}
-		}
-	}
 }
