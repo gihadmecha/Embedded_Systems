6501b3dc87f47e31e52357f997ee52f1d1e72e4f
gihadmecha <gihadmecha@gmail.com>
2023-05-26 07:11:33 +0300

    DIO driver

--- atmega32_ECU/atmega32_ECU/Release/makedep.mk
+++ atmega32_ECU/atmega32_ECU/Release/makedep.mk
@@ -2,11 +2,5 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-BUTTON.c
-
-LED.c
-
 main.c
-
-SEGMENTS.c
 
